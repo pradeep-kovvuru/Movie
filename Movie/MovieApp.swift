9d08e0af0a92049bbebd17c5d09493ba2baec996
Pradeep Kovvuru <kovvuru.reddy@mtrustcompany.com>
2024-07-30 19:19:54 +0530

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Reddy, Kovvuru Pradeep Kumar (Cognizant) on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
